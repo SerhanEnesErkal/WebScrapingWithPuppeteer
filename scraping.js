be8cb52ee2d5dca8b88e075232dde705d2b2a828
Serhan Enes <serhaneneserkal06@gmail.com>
2022-08-19 14:11:18 +0300

    11

--- scraping.js
+++ scraping.js
@@ -12,27 +12,20 @@ const puppeteer = require('puppeteer');
    let page = await browser.newPage();
 
    await page.goto(url, {waitUntil: 'networkidle2' });
-
+   
    let data = await page.evaluate(()=>{ 
-    let altın= document.querySelector("body > header > div.header-secondary > div > div.market-data > div:nth-child(1) > a > span.value").innerText
-    let dolar= document.querySelector("body > header > div.header-secondary > div > div.market-data > div:nth-child(2) > a > span.value").innerText
-    let euro= document.querySelector("body > header > div.header-secondary > div > div.market-data > div:nth-child(3) > a > span.value").innerText
-    let sterlin= document.querySelector("body > header > div.header-secondary > div > div.market-data > div:nth-child(4) > a > span.value").innerText
-    let BIST100= document.querySelector("body > header > div.header-secondary > div > div.market-data > div:nth-child(5) > a > span.value").innerText
-    let bitcoin= document.querySelector("body > header > div.header-secondary > div > div.market-data > div:nth-child(6) > a > span.value").innerText
-    let gümüş= document.querySelector("body > header > div.header-secondary > div > div.market-data > div:nth-child(7) > a > span.value").innerText
-    let faiz= document.querySelector("body > header > div.header-secondary > div > div.market-data > div:nth-child(8) > a > span.value").innerText
+      genel=[]
+      for(value=1;value<=8;value++){
+          let a= document.querySelector("body > header > div.header-secondary > div > div.market-data > div:nth-child("+value+")> a > span.value").innerText
+          if (value=1)  { genel.push("altinin degeri:"+a)}
+          
+         else{continue;}
 
-      return{
-         altın,
-         dolar,
-         euro,
-         sterlin,
-         BIST100,
-         bitcoin,
-         gümüş,
-         faiz      
-      }      
+
+             }
+      return{genel
+      
+      }
    });
   console.log(data);
   await browser.close();
